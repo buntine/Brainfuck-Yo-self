adef2052906699ab34fa7c5e65cf931a34ed86b0
Andy Buntine <info@andrewbuntine.com>
2009-08-28 23:19:08 +1000

    Updated bundled script and README

--- brainfuck.py
+++ brainfuck.py
@@ -1,6 +1,14 @@
 #! /usr/bin/env python
 
+# A quick little wrapper script to execute a brainfuck program.
+
+import sys
 from lib.interpreter import BrainFuck
 
-bf = BrainFuck("./examples/hello_world.bf")
+if len(sys.argv) != 2:
+    print "Usage: ./brainfuck.py path/to/a/file.bf"
+    sys.exit(1)
+
+bf = BrainFuck(sys.argv[1])
 bf.interpret()
+print
